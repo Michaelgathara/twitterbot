d648b8ef2db397ca1cdf54b47afcfa139c8b6740
michaelgathara <mikegtrm@gmail.com>
2020-07-18 04:01:03 -0500

    New Function

--- main.py
+++ main.py
@@ -59,7 +59,8 @@ def fav_retweet(api):
         if mention.in_reply_to_status_id is not None or mention.user.id == api.me().id:
             # This tweet is a reply or I'm its author so, ignore it
             return
-        
+        # 
+        # if mention.in_reply_to_status_id is mention.user.id == api.me().id:
         if not mention.favorited:
             # Mark it as Liked, since we have not done it yet
             try:
@@ -75,6 +76,32 @@ def fav_retweet(api):
                 logger.info(f"Retweeted tweet by {mention.user.name}")
             except Exception as e:
                 logger.error("Error on fav and retweet", exc_info=True)
+
+def all_retweet(api):
+    logger.info('Retrieving tweets...')
+    mentions = api.mentions_timeline(tweet_mode = 'extended')
+    for mention in reversed(mentions):
+        if mention.in_reply_to_status_id is mention.user.id == api.me().id:
+            # This tweet is a reply or I'm its author so, ignore it
+            return
+        # 
+        # if mention.in_reply_to_status_id is mention.user.id == api.me().id:
+        if not mention.favorited:
+            # Mark it as Liked, since we have not done it yet
+            try:
+                mention.favorite()
+                logger.info(f"Liked tweet by {mention.user.name}")
+            except Exception as e:
+                logger.error("Error on fav", exc_info=True)
+                
+        if not mention.retweeted:
+            # Retweet, since we have not retweeted it yet
+            try:
+                mention.retweet()
+                logger.info(f"Retweeted tweet by {mention.user.name}")
+            except Exception as e:
+                logger.error("Error on fav and retweet", exc_info=True)
+
 def retweet_tweets_with_ticker(api, need_ticker):
     if type(need_ticker) is list:
         search_query = f"{need_ticker} -filter:retweets"
@@ -97,6 +124,8 @@ def retweet_tweets_with_ticker(api, need_ticker):
 def main():
     api = create_api()
     while True:
+        fav_retweet(api)
+        all_retweet(api)
         retweet_tweets_with_hashtag(api, ["#manim"])
         fav_retweet_user(api, "@sybersid_18")
         retweet_tweets_with_ticker(api, ["$tsla"])
