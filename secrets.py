c7f46ebe0a0cb9cecd926cd5f5788a149d0b0d78
michaelgathara <mikegtrm@gmail.com>
2020-07-30 03:36:59 -0500

    Heroku setup

--- secrets.py
+++ secrets.py
@@ -1,5 +1,11 @@
-key = [XXXXXXX,
-      XXXXXXX,
-      XXXXXXX,
-      XXXXXXX
+from os import environ
+CONSUMER_KEY = environ['consumer_key']
+CONSUMER_SECRET = environ['consumer_secret']
+ACCESS_KEY = environ['access_token']
+ACCESS_TOKEN_SECRET = environ['access_token_secret']
+
+key = [CONSUMER_KEY,
+      CONSUMER_SECRET,
+      ACCESS_KEY,
+      ACCESS_SECRET
 ]
